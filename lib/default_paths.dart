cbad05b3f164d9a5453fb80897f9f456a861da9c
glbondiii <glbondiii@protonmail.com>
2023-11-22 22:32:07 -0600

    Clarified instructions for default_paths.dart

--- lib/default_paths.dart
+++ lib/default_paths.dart
@@ -1,4 +1,5 @@
 // COPY THIS FILE AS "paths.dart" AND CHANGE THESE ACCORDING TO YOUR FILE STRUCTURE
+// THIS FILE WILL NOT BE READ IN THE PROGRAM SO COPYING THIS FILE IS REQUIRED
 String softwarePath = "";
 String usersPath = "$softwarePath/users";
 String examsPath = "$softwarePath/exams";
