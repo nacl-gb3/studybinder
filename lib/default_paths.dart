67f1653ecbe15f87e895f01ac18384496d4de79c
glbondiii <glbondiii@protonmail.com>
2023-11-22 22:28:03 -0600

    Changed how paths.dart works

---
+++ lib/default_paths.dart
@@ -0,0 +1,5 @@
+// COPY THIS FILE AS "paths.dart" AND CHANGE THESE ACCORDING TO YOUR FILE STRUCTURE
+String softwarePath = "";
+String usersPath = "$softwarePath/users";
+String examsPath = "$softwarePath/exams";
+
