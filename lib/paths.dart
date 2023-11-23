0915469bbad0e9f00ddc5cc4a925dd915fffeaba
glbondiii <glbondiii@protonmail.com>
2023-11-22 22:29:58 -0600

    Removed pure paths.dart

--- lib/paths.dart
+++
@@ -1,5 +0,0 @@
-// CHANGE THESE ACCORDING TO YOUR FILE STRUCTURE
-String softwarePath = "/home/glbondiii/Programming_Projects/studybinder";
-String usersPath = "$softwarePath/users";
-String examsPath = "$softwarePath/exams";
-
